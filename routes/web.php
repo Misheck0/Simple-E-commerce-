bcdfd61f7782e54cbc40341587d24af812f643a2
misheck <misheck@localhost.localdomain>
2024-06-16 06:58:19 -0400

    first load app file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PhoneController;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [PhoneController::class,'index']);
+
+Route::get('/re',function(){
+
+return view('register');
+
 });
+Route::get('/login_form',function(){
+     return view('login');
+   });
+//get form register dat
+Route::post("/register",[UserController::class,'register'])->name('new_user');
+Route::post('/login',[UserController::class,'login']);
+
